b5f819a8959fe3fdcbce13f85eaef480cced9a25
Adeola23 <amosadeola92@gmail.com>
2021-02-08 22:50:51 +0100

    move goal

--- adeolascrumy/urls.py
+++ adeolascrumy/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from adeolascrumy import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('movegoal/<int:goal_id>', views.move_goal)
 ]
--- adeolascrumy/views.py
+++ adeolascrumy/views.py
@@ -6,6 +6,10 @@ from .models import ScrumyGoals
 def index(request):
     goal = ScrumyGoals.objects.filter(goal_name = 'Learn Django')
     return HttpResponse (goal)
+
+def move_goal(request, **kwargs):
+    output = ScrumyGoals.objects.get(goal_id = kwargs['goal_id'])
+    return HttpResponse (output)
     
     #return HttpResponse('This is a Scrum Application')
 # Create your views here.
