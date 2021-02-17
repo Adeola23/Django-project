10f033054e220fd1b2e38044e892a3fc77f1f491
Adeola23 <amosadeola92@gmail.com>
2021-02-17 14:58:52 +0100

    included namespace

--- adeolascrumy/urls.py
+++ adeolascrumy/urls.py
@@ -3,7 +3,7 @@ from adeolascrumy import views
 app_name = 'adeolascrumy'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('movegoal/<int:goal_id>', views.move_goal),
     path('addgoal/', views.add_goal),
     path('home/', views.home),
--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('adeolascrumy/', include('adeolascrumy.urls', namespace='adeolascrumy'))
+    path('adeolascrumy/', include('adeolascrumy.urls')),
+    
 
 
 ]
